7f17dcccf0ef4b14cfe9bfc4b2c39b3ec915044a
Marcin Pietras <martinpie10@gmail.com>
2021-01-05 18:21:06 +0100

    create array isRate

--- api/models/exercise.js
+++ api/models/exercise.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
 
+exerciseRate = {
+    counter: { type: Number },
+    sum: { type: Number },
+    rate: { type: Number }
+}
+
 const exerciseSchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
     name: { type: String, required: true },
     type: { type: String, required: true },
     description: { type: String, required: true },
-    rate: { type: Number, default: 0 },
+    rate: {
+        type: exerciseRate, default: {
+            counter: 0,
+            sum: 0,
+            rate: 0
+        }
+    },
     popular: { type: Number, default: 0 },
-    musclePart: {type: String, required: true},
-    image: {type: String, required: true },
-    video: {type: String, default: ""},
+    musclePart: { type: String, required: true },
+    image: { type: String, required: true },
+    video: { type: String, default: "" },
     difficult: { type: Number, default: 0 },
     kcalRatio: { type: Number, default: 0 }
 });
